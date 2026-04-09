2f43b17e5b04cfcb3bc32d0080d6f2b66592df5d
Chaustrej <131916274+Chaustrej@users.noreply.github.com>
2026-04-09 18:56:37 -0400

    algoritmos de fifo, lifo y round robin en camino

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@
 using namespace std;
 using namespace std::chrono;
 
-// --- ESTRUCTURA DE DATOS ---
 struct Actividad {
     string id;
     int ti, t, tf, T;
@@ -18,7 +17,7 @@ struct Actividad {
     double I;
 };
 
-// --- CARGA DE DATOS (data/Datos.csv) ---
+// Carga de datos desde data/Datos.csv 
 void cargarDatos(string ruta, vector<string> &ids, vector<int> &ti, vector<int> &t) {
     ifstream archivo(ruta);
     if (!archivo) {
@@ -27,13 +26,11 @@ void cargarDatos(string ruta, vector<string> &ids, vector<int> &ti, vector<int> 
     }
     string linea;
     bool primeraLinea = true;
-
     while (getline(archivo, linea)) {
         if (linea.empty()) continue;
         stringstream ss(linea);
         string id_val, ti_val, t_val;
         if (getline(ss, id_val, ',') && getline(ss, ti_val, ',') && getline(ss, t_val)) {
-            // Salto inteligente de encabezado
             if (primeraLinea && (ti_val.find_first_not_of("0123456789") != string::npos)) {
                 primeraLinea = false;
                 continue;
@@ -46,9 +43,8 @@ void cargarDatos(string ruta, vector<string> &ids, vector<int> &ti, vector<int> 
     }
 }
 
-// --- MÉTRICAS E IMPRESIÓN ---
-// Ahora retorna el pT para la comparativa final
-double obtenerResultados(string nombre, const vector<string> &ids, const vector<int> &ti, const vector<int> &t, const vector<int> &tf, double micro) {
+// Procesa métricas y retorna pE para la comparativa 
+double obtenerPE(string nombre, const vector<string> &ids, const vector<int> &ti, const vector<int> &t, const vector<int> &tf, double micro) {
     int n = ids.size();
     double sumT = 0, sumI = 0;
     long long sumE = 0;
@@ -72,10 +68,81 @@ double obtenerResultados(string nombre, const vector<string> &ids, const vector<
         sumT += T_val; sumE += E_val; sumI += I_val;
     }
 
-    double pT = sumT / n;
+    double pE = (double)sumE / n;
     cout << string(70, '-') << "\n";
-    cout << " PROMEDIOS: pT = " << pT << " | pE = " << (double)sumE/n << " | pI = " << sumI/n << "\n";
+    cout << " PROMEDIOS: pT = " << sumT/n << " | pE = " << pE << " | pI = " << sumI/n << "\n";
     cout << " TIEMPO DE CALCULO: " << micro << " microsegundos\n";
-    return pT; // Retornamos pT para comparar
+    return pE; 
 }
 
+// Algoritmo FIFO
+vector<int> ejecutarFIFO(const vector<int> &ti, const vector<int> &t) {
+    int n = ti.size();
+    vector<int> tf(n, 0);
+    vector<bool> done(n, false);
+    int t_clock = 0, completados = 0;
+    while (completados < n) {
+        bool encontrado = false;
+        for (int i = 0; i < n; i++) {
+            if (!done[i] && ti[i] <= t_clock) {
+                t_clock += t[i];
+                tf[i] = t_clock;
+                done[i] = true;
+                completados++;
+                encontrado = true;
+                break; 
+            }
+        }
+        if (!encontrado) t_clock++; 
+    }
+    return tf;
+}
+// Algoritmo LIFO
+
+vector<int> ejecutarLIFO(const vector<int> &ti, const vector<int> &t) {
+    int n = ti.size();
+    vector<int> tf(n, 0);
+    vector<bool> done(n, false);
+    int t_clock = 0, completados = 0;
+    while (completados < n) {
+        bool encontrado = false;
+        for (int i = n - 1; i >= 0; i--) { 
+            if (!done[i] && ti[i] <= t_clock) {
+                t_clock += t[i];
+                tf[i] = t_clock;
+                done[i] = true;
+                completados++;
+                encontrado = true;
+                break;
+            }
+        }
+        if (!encontrado) t_clock++;
+    }
+    return tf;
+}
+// Algoritmo Round robin con quantum q = 4
+
+vector<int> ejecutarRR(const vector<int> &ti, const vector<int> &t, int q) {
+    int n = ti.size();
+    vector<int> tf(n, 0), rem = t;
+    vector<bool> done(n, false);
+    int t_clock = 0, completados = 0;
+    while (completados < n) {
+        bool encontrado_ciclo = false;
+        for (int i = 0; i < n; i++) {
+            if (!done[i] && ti[i] <= t_clock) {
+                int run_time = (rem[i] < q) ? rem[i] : q; 
+                t_clock += run_time;
+                rem[i] -= run_time;
+                encontrado_ciclo = true;
+                if (rem[i] == 0) {
+                    done[i] = true;
+                    tf[i] = t_clock;
+                    completados++;
+                }
+            }
+        }
+        if (!encontrado_ciclo) t_clock++;
+    }
+    return tf;
+}
